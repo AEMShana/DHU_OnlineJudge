35adbfda72fa3c7cd125c845666eb8e8a93738a0
Contna <657844137@qq.com>
2022-04-02 11:21:35 +0800

    update

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ pip install bs4
 pip install lxml
 pip install pymdown-extensions
 pip install Pillow
+pip install django-ckeditor
+pip install django-mptt
 ```
--- dhuoj/dhuoj/settings.py
+++ dhuoj/dhuoj/settings.py
@@ -132,8 +132,8 @@ CKEDITOR_CONFIGS = {
     # django-ckeditor默认使用default配置
     'default': {
         # 编辑器宽度自适应
-        'width':'auto',
-        'height':'250px',
+        'width': 'auto',
+        'height': '250px',
         # tab键转换空格数
         'tabSpaces': 4,
         # 工具栏风格
@@ -141,7 +141,7 @@ CKEDITOR_CONFIGS = {
         # 工具栏按钮
         'toolbar_Custom': [
             # 表情 代码块
-            ['Smiley', 'CodeSnippet'], 
+            ['Smiley', 'CodeSnippet'],
             # 字体风格
             ['Bold', 'Italic', 'Underline', 'RemoveFormat', 'Blockquote'],
             # 字体颜色
