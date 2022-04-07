80b670a9f012c40974035c0e8201ab64b7c4f6d3
Contna <657844137@qq.com>
2022-04-07 17:57:32 +0800

    update

--- dhuoj/home/views.py
+++ dhuoj/home/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from blog.models import ArticlePost
+from blog.models import ArticlePost, ArticleColumn
 from problemset.models import Problem
 
 
 def home(request):
     new_article = ArticlePost.objects.all()[:3]
     new_problem = Problem.objects.all()[:3]
+    news = ArticlePost.objects.filter(column__title="新闻")[:3]
+    print(news)
     context = {
         "new_article": new_article,
         "new_problem": new_problem,
+        "news": news
     }
     return render(request, 'home/index.html', context)
